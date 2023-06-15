5126f0a183847fee4d37cc85fba7f0f7b54cbadf
josefalaporte <laportejosefa@gmail.com>
2023-06-15 16:54:31 -0400

    newinfo

--- app.js
+++ app.js
@@ -65,7 +65,15 @@ console.log(3 + 5);
     }
 
     //*Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a(pantalla) 9
-        
 
-  
+    function EncuentraLetra()
+    let frase = prompt("Ingresa una frase: ");
 
+    let contador = 0
+    for (let i = 0; i <frase.length; i++) {
+        if (frase [i] === "a"){
+
+        }
+    }
+    //*resultado
+    console.log("La letra 'a' aparece", contador, "veces");
